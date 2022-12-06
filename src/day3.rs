4ed6f8d27d6f1e22341c5e3ad447e864f95f3afe
Jonas Dietrich <diejoc20@htl-kaindorf.at>
2022-12-06 20:43:18 +0100

    Started day 3

--- src/day3.rs
+++ src/day3.rs
@@ -7,4 +7,26 @@ pub fn solve() {
     
     let file = File::open("res/day3.txt").unwrap();
     let reader = BufReader::new(file);
+
+    let mut score: u32 = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let left_part = &line[0..line.len()/2];
+        let right_part= &line[line.len()/2..line.len()];
+
+        for i in left_part.chars() {
+            let mut found_chars:Vec<char> = Vec::new();
+            for j in right_part.chars() {
+                if i == j {
+                    if !found_chars.contains(&j) {
+                        if i.is_ascii_lowercase() {score += i as u32 - 96}
+                        else  {score += i as u32 - 38}
+                        found_chars.push(j);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Solution 1: {}", score);
 }
